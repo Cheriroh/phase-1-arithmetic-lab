1c53a8615307867030907533ac65713dcb425168
Cheriroh <prudencec2019@gmail.com>
2024-12-07 22:11:11 +0300

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+//Multiplication to get a result of 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;// 31 * 2 = 62
+ 
 
-// Write your code here
+// Generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1; 
+// Calculate the remainder to get 4
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4; 
+
+// Find the highest number in a set
+let max= Math.max(7,9,2,12,20)
